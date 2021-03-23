a6089888d66477d5a1b9e6613f0e0878e661676d
jirathon.chaisuk <jirathonchaisuk@kkumail.com>
2021-03-24 03:24:29 +0700

    :boom: create class DayCounter :boom:

--- demo1/src/sample/controller/daycounter/DaycounterController.java
+++ demo1/src/sample/controller/daycounter/DaycounterController.java
@@ -1,5 +1,6 @@
 package sample.controller.daycounter;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,6 +11,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import sample.item.DayCounter;
 
 import java.io.IOException;
 import java.net.URL;
@@ -21,11 +24,34 @@ public class DaycounterController implements Initializable {
     public TextField textInput;
     public Label outputLabel;
     public DatePicker dayInput;
+    public Label dayLabel;
     public static LocalDate dayNow = LocalDate.now();
+    public static Label static_day_label;
     public Label todayNow;
     public static int dateLeft;
-    public static Label static_day_label;
-    public Label dayLabel;
+    private static double x,y;
+    public TextField titleEvent;
+    public DatePicker dateEvent;
+    public TextField descriptionEvent;
+
+    public static void dragWidget(Parent root,Stage primaryStage) {
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                x = event.getSceneX();
+                y = event.getSceneY();
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX()-x);
+                primaryStage.setY(event.getScreenY()-y);
+            }
+        });
+    }
 
     @FXML
     public void openDaycounter(MouseEvent mouseEvent) {
@@ -38,18 +64,15 @@ public class DaycounterController implements Initializable {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../views/daycounterWidget.fxml"));
             Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
+            dragWidget(root1,stage);
             stage.setTitle("daycounter");
             stage.setScene(new Scene(root1));
+            stage.initStyle(StageStyle.TRANSPARENT);
             stage.show();
             static_day_label.setText(textInput.getText());
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        static_day_label = dayLabel;
     }
 
     public void onClick(MouseEvent mouseEvent) {
@@ -58,4 +81,31 @@ public class DaycounterController implements Initializable {
         System.out.println(dayNow);
         System.out.println(dateLeft);
     }
+
+    public void addPageDaycounter(MouseEvent mouseEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../views/addDayCounterEvents.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Add Day Counter");
+            dragWidget(root1,stage);
+            stage.setScene(new Scene(root1));
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addDaycounter(MouseEvent mouseEvent) {
+        DayCounter ter = new DayCounter(titleEvent.getText(),dateEvent.getValue(),descriptionEvent.getText());
+        System.out.println(ter);
+        ter.calculationDayLeft();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        static_day_label = dayLabel;
+    }
 }
+
--- demo1/src/sample/item/DayCounter.java
+++ demo1/src/sample/item/DayCounter.java
@@ -6,34 +6,50 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class DayCounter {
-    private DatePicker dayStart;
-    private Date dayEnd;
+    private String title;
+    private LocalDate dayEnd;
+    private LocalDate dayStart;
     private String description;
     private LocalDate now = LocalDate.now();
 
-    public DayCounter(DatePicker dayStart, Date dayEnd, String description) {
-        this.dayStart = dayStart;
+    public DayCounter(String title,LocalDate  dayEnd, String description) {
+        this.title = title;
         this.dayEnd = dayEnd;
         this.description = description;
+        this.dayStart = now;
+    }
+
+    @Override
+    public String toString() {
+        return "DayCounter{" +
+                "title='" + title + '\'' +
+                ", dayEnd=" + dayEnd +
+                ", dayStart=" + dayStart +
+                ", description='" + description + '\'' +
+                ", now=" + now +
+                '}';
     }
 
     public long calculationDayLeft() {
         long diff = 0;
         SimpleDateFormat myFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String inputString1 = String.valueOf(dayStart.getValue());
-        String inputString2 = String.valueOf(dayEnd.getTime());
-
+        String inputString1 = String.valueOf(dayStart);
+        String inputString2 = String.valueOf(dayEnd);
+        System.out.println(inputString1);
+        System.out.println(inputString2);
         try {
             Date date1 = myFormat.parse(inputString1);
             Date date2 = myFormat.parse(inputString2);
-            diff = date2.getTime() - date1.getTime();
+            diff = date2.getTime() - date1.getTime(); //chrono unit
+            diff = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // day unit
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
-            return diff;
-
+        return diff;
     }
 
 }
