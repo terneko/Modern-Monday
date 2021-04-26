edaeec4bcd90cba948441ef0f717f6048fcdab6d
Aphichit-Anthapanya <aphichit_atpy@kkumail.com>
2021-04-27 02:16:20 +0700

    date and clock uka uka uka
    uka

---
+++ demo1/src/main/java/sample/controller/dateandclock/widgetDateAndClockV3.java
@@ -0,0 +1,77 @@
+package sample.controller.dateandclock;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class widgetDateAndClockV3 implements Initializable {
+    public Label day2;
+    public Label dayInWeekAndMonthAndYear2;
+    public Label dayInweakAndMonthAndDay3;
+    public Label year3;
+    public Label dayInWeakAndDay4;
+    public Label monthAndYear4;
+    public Label hClock1;
+    public Label minuteClock1;
+
+
+    public Label dateTop1;
+    public Label dateBottom1;
+
+    public Label day2Widget;
+    public Label dayInWeekAndMonthAndYear2Widget;
+
+    public Label dayInweakAndMonthAndDay3Widget;
+    public Label year3Widget;
+
+    public Label dayInWeakAndDay4Widget;
+    public Label monthAndYear4Widget;
+
+    String[] oneWeek = {
+            "Sunday"
+            , "Monday"
+            , "Tuesday"
+            , "Wednesday"
+            , "Thursday"
+            , "Friday"
+            , "Saturday"};
+    String[] oneYear = {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+    };
+
+    private void setDate() {
+        Date d = new Date();
+        SimpleDateFormat year = new SimpleDateFormat("yyyy");
+        SimpleDateFormat month = new SimpleDateFormat("MM");
+        SimpleDateFormat day = new SimpleDateFormat("dd");
+
+        dayInweakAndMonthAndDay3Widget.setText(oneWeek[d.getDay()].substring(0,3)+", "+oneYear[d.getMonth()]+day.format(d));
+        year3Widget.setText(year.format(d));
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setDate();
+    }
+}
